4de171e7075e60a4683d3a7faf51214ff3391440
fankainang <417432483@qq.com>
2023-01-27 00:20:41 +0800

    finish 2-1

--- .lnvim.fnl
+++
@@ -1,5 +0,0 @@
-(module scip-local
-  {require {nvim nvim-local-fennel.aniseed.nvim}})
-
-(set nvim.g.conjure#client#guile#socket#pipename ".guile-repl.socket")
-
--- scm/guile.sh
+++ scm/guile.sh
@@ -4,5 +4,5 @@ set -xe
 
 SOCKET=$(git rev-parse --show-toplevel)/.guile-repl.socket
 if [ -f $SOCKET ]; then rm $SOCKET; fi
-guile --listen=$SOCKET
+guile --listen=$SOCKET --no-auto-compile
 rm $SOCKET
